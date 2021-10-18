4802629392339d9e682c8895b8da514e603bef33
unknown <ansulaoari88@gmail.com>
2021-10-18 19:56:05 +0530

    added hashing passwords technique

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
 const app =  express();
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields: ["password"]  });
+
 const User = mongoose.model("User",userSchema);
 
 
@@ -38,7 +38,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save((err)=>{
         if(err){
@@ -52,7 +52,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},(err,foundUser)=>{
         if(err){
